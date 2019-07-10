e8a021184c8124641e8851d705725314665c58f9
RkEclair <ryosukadnak@gmail.com>
2019-07-11 01:08:14 +0900

    Lint & modify parameter

--- example.js
+++ example.js
@@ -1,30 +1,29 @@
 const deream = require('./index');
-const { createCanvas } = require('canvas');
+const {createCanvas} = require('canvas');
 
 const streamIn = deream({
-    args: {
-        // ffmpeg options
-        'y' : '',
-        'c:v': 'libx264',
-        'b:v': '1024k',
-        'r:v': '30'
-    },
-    // path to destination file
-    dest: 'dest.mp4',
+  args: {
+    // ffmpeg options
+    y: '',
+    'c:v': 'libx264',
+    'b:v': '1024k',
+    'r:v': '30'
+  },
+  // path to destination file
+  dest: 'dest.mp4'
 });
-
 
 const canvas = createCanvas(640, 360);
 const context = canvas.getContext('2d');
 
 // make 2seconds/30fps movie
 for (let i = 0; i < 60; i++) {
-    const frameHex = `00${i.toString(16)}`.slice(-2);
-    context.fillStyle = ['#', frameHex, frameHex, frameHex].join('');
-    context.fillRect(0, 0, 640, 360);
+  const frameHex = `00${i.toString(16)}`.slice(-2);
+  context.fillStyle = ['#', frameHex, frameHex, frameHex].join('');
+  context.fillRect(0, 0, 640, 360);
 
-    // write PNG Buffer
-    streamIn.write(canvas.toBuffer());
+  // write PNG Buffer
+  streamIn.write(canvas.toBuffer());
 }
 
 streamIn.end();
--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const duplexer = require('duplexer3');
 const {spawn} = require('child_process');
 
-const isObject = target => Object.getPrototypeOf(target) === Object.prototype;
-const hasKey = (target, property) => ({}).hasOwnProperty.call(target, property);
-const entries = object => Object.keys(object).map(key => [key, object[key]]);
+const isObject = (target) => Object.getPrototypeOf(target) === Object.prototype;
+const hasKey = (target, property) => ({}.hasOwnProperty.call(target, property));
+const entries = (object) =>
+  Object.keys(object).map((key) => [key, object[key]]);
 
-const parseArgs = args => {
-    const ignoreKeys = ['y'];
+const parseArgs = (args) => {
+  const ignoreKeys = ['y'];
 
-    if (typeof args === 'string') {
-        return args.split(' ');
-    }
-    else if (isObject(args)) {
-        const _args = [];
+  if (typeof args === 'string') {
+    return args.split(' ');
+  } else if (isObject(args)) {
+    const _args = [];
 
-        for (const [key, value] of entries(args)) {
-            if (ignoreKeys.indexOf(key) !== -1) {
-                continue;
-            }
+    for (const [key, value] of entries(args)) {
+      if (ignoreKeys.indexOf(key) !== -1) {
+        continue;
+      }
 
-            _args.push(`-${key}`);
-            value !== '' && _args.push(value);
-        }
-
-        return _args;
+      _args.push(`-${key}`);
+      value !== '' && _args.push(value);
     }
 
-    return args;
-}
+    return _args;
+  }
+
+  return args;
+};
 
 /**
  * @param {Object} options
@@ -37,29 +37,31 @@ const parseArgs = args => {
  *      Ignore other all options ignored when args specified.
  */
 module.exports = function deream(options = {}) {
-    const _options = Object.assign({
-        args: null,
-        dest: null,
-    }, options);
+  if (options.args === null || options.args === undefined) {
+    throw new Error('options.args must be specified');
+  }
 
-    if (options.args === null) {
-        throw new Error('options.args must be specified');
-    }
+  if (options.dest === null || options.dest === undefined) {
+    throw new Error('options.args must be specified');
+  }
 
-    if (options.dest === null) {
-        throw new Error('options.args must be specified');
-    }
+  const specificArgs = parseArgs(options.args);
+  const args = [
+    '-i',
+    'pipe:0',
+    '-c:v',
+    'png_pipe',
+    ...specificArgs,
+    options.dest ? options.dest : 'pipe:1'
+  ];
 
-    const specificArgs = parseArgs(_options.args);
-    const args = ['-i', 'pipe:0', '-c:v', 'png_pipe', ...specificArgs, options.dest ? options.dest : 'pipe:1'];
+  if (hasKey(options.args, 'y')) {
+    args.unshift('-y');
+  }
 
-    if (hasKey(_options.args, 'y')) {
-        args.unshift('-y');
-    }
+  console.log(args.join` `);
 
-    console.log(args.join` `);
-
-    const ffmpeg = spawn('ffmpeg', args);
-    ffmpeg.stderr.on('data', chunk => console.log(chunk.toString()));
-    return duplexer(ffmpeg.stdin, ffmpeg.stdout);
+  const ffmpeg = spawn('ffmpeg', args);
+  ffmpeg.stderr.on('data', (chunk) => console.log(chunk.toString()));
+  return duplexer(ffmpeg.stdin, ffmpeg.stdout);
 };
